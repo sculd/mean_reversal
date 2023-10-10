0ef73b0ba55121cdb4e90a55ca3d46704f719038
Hyungjun Lim <sculd3@gmail.com>
2023-10-10 15:31:28 -0400

    algo/trading/trade.py

--- algo/trading/trade.py
+++ algo/trading/trade.py
@@ -1,4 +1,5 @@
 import pandas as pd, numpy as np
+import logging
 
 import algo.minimal_predictability.calculate
 import algo.statarbitrage.bband
@@ -25,19 +26,29 @@ class Status:
     def __init__(self, weight):
         self.weight = weight
 
-    def init_status():
-        df_prices = algo.trading.prices.get_df_prices()
+    def init_status(df_prices):
         _, var_eigen_vecs, wgts = algo.minimal_predictability.calculate.get_var1_wgts_values_transpose(*df_prices.values.T)
-        status = Status(wgts[:0])
+        status = Status(wgts[:,0])
         return status
 
 
-class Trading:
+class TradeManager:
     def __init__(self, symbols):
-        self.status = Status.init_status()
         self.price_cache = algo.trading.prices.PriceCache(symbols, default_max_window)
-        self.trading_param = TradingParam.get_default_param()
+        self.status = Status.init_status(self.price_cache.get_df_prices())
+        self.trading_param = TradingParam.get_default_param(symbols)
         self.last_rebalance_epoch_seconds = 0
+        self.df_prices = self.price_cache.get_df_prices()
+        self.on_price_update()
+
+
+    def tick(self):
+        print('tick')
+        df_prices = self.price_cache.get_df_prices()
+        price_updated = self.df_prices.index[-1].to_datetime64() == df_prices.index[-1].to_datetime64()
+        self.df_prices = df_prices
+        if price_updated:
+            self.on_price_update()
 
 
     def on_price_update(self):
@@ -47,34 +58,35 @@ class Trading:
         get if position changed
         if position changes, execute the position change.
         '''
+        print('on_price_update')
         if self.get_if_rebalance():
             self.rebalance_weight()
 
         position_changed = self.get_position_changed()
         if position_changed:
+            print('[on_price_update] position has changed')
             pass
 
 
     def get_if_rebalance(self):
-        df_prices = self.price_cache.get_df_prices()
-        recent_epoch_seconds = df_prices.index[-1].to_datetime64().astype('int') // 10**9
+        recent_epoch_seconds = self.df_prices.index[-1].to_datetime64().astype('int') // 10**9
         if recent_epoch_seconds < self.last_rebalance_epoch_seconds + self.trading_param.rebalance_period_minutes * 60:
             return False
         return True
 
 
     def rebalance_weight(self):
-        df_prices = self.price_cache.get_df_prices()
-        _, var_eigen_vecs, wgts = algo.minimal_predictability.calculate.get_var1_wgts_values_transpose(*df_prices.values.T)
-        self.status.weight = wgts[:0]
-        last_rebalance_epoch_seconds = df_prices.index[-1].to_datetime64().astype('int') // 10**9
+        print('rebalance_weight')
+        _, var_eigen_vecs, wgts = algo.minimal_predictability.calculate.get_var1_wgts_values_transpose(*self.df_prices.values.T)
+        self.status.weight = wgts[:,0]
+        last_rebalance_epoch_seconds = self.df_prices.index[-1].to_datetime64().astype('int') // 10**9
         # gracefully anchor (5 minutes)
         last_rebalance_epoch_seconds = int(last_rebalance_epoch_seconds // 300) * 300
         self.last_rebalance_epoch_seconds = last_rebalance_epoch_seconds
 
 
     def get_position_changed(self):
-        df_prices = self.price_cache.get_df_prices()
-        df_features = algo.statarbitrage.bband.add_features(df_prices, self.status.weight, self.trading_param.bband_trading_param)
+        print('get_position_changed')
+        df_features = algo.statarbitrage.bband.add_features(self.df_prices, self.status.weight, self.trading_param.bband_trading_param)
         return df_features.iloc[-1].position_changed
 
