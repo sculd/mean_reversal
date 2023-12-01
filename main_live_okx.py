ee7f9d95fc1fe7ce354b456e24ee4f3a13b1b69e
Hyungjun Lim <sculd3@gmail.com>
2023-11-30 21:41:19 -0500

    use live execution for okx

--- main_live_okx.py
+++ main_live_okx.py
@@ -21,8 +21,8 @@ class Live:
     def __init__(self, symbols):
         self.trading_param = algo.trading.trade.StatArbitrageTradingParam.get_default_param()
         self.price_cache = algo.trading.prices_okx.PriceCache(symbols, self.trading_param.get_max_window_minutes())
-        self.trade_execution = None
-        #self.trade_execution = algo.trading.execution_okx.TradeExecution(symbols, target_betsize=100)
+        #self.trade_execution = None
+        self.trade_execution = algo.trading.execution_okx.TradeExecution(symbols, target_betsize=100)
         self.trade_manager = algo.trading.trade.TradeManager(symbols, price_cache=self.price_cache, trade_execution=self.trade_execution)
         self.cycle = algo.trading.cycle.Cycle(self.trade_manager, 10)
 
